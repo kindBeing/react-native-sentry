289b2e543df18026003cf0eec9922f45949a60d2
kindBeing <nspidunite@gmail.com>
2023-07-02 18:51:43 -0700

    Add react-native-dotenv and use env variables

--- App.tsx
+++ App.tsx
@@ -11,9 +11,13 @@ import {
   ScrollView,
   StatusBar,
   StyleSheet,
+  Text,
   useColorScheme,
   View,
 } from 'react-native';
+// @ts-ignore
+// eslint-disable-next-line prettier/prettier
+import { SENTRY_DSN, SENTRY_SAMPLE_RATE, SENTRY_ORG, SENTRY_PROJECT, SENTRY_AUTH_TOKEN, SENTRY_RELEASE, SENTRY_DIST } from '@env';
 
 import {Colors, Header} from 'react-native/Libraries/NewAppScreen';
 import CustomButton from './nested-directory/CustomButton';
@@ -40,6 +44,22 @@ function App(): JSX.Element {
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
           <CustomButton />
+          <View>
+            <Text>Sentry DSN: {SENTRY_DSN}</Text>
+            <Text>{'\n'}</Text>
+            <Text>Sentry Sample Rate: {SENTRY_SAMPLE_RATE}</Text>
+            <Text>{'\n'}</Text>
+            <Text>Sentry Org: {SENTRY_ORG}</Text>
+            <Text>{'\n'}</Text>
+            <Text>Sentry Project: {SENTRY_PROJECT}</Text>
+            <Text>{'\n'}</Text>
+            <Text>Sentry Auth Token: {SENTRY_AUTH_TOKEN}</Text>
+            <Text>{'\n'}</Text>
+            <Text>Sentry Release: {SENTRY_RELEASE}</Text>
+            <Text>{'\n'}</Text>
+            <Text>Sentry Dist: {SENTRY_DIST}</Text>
+            <Text>{'\n'}</Text>
+          </View>
         </View>
       </ScrollView>
     </SafeAreaView>
